b0e321d58cb624cd205004b3e972704af1e97071
Richards Griezitis <ricis1818@gmail.com>
2021-06-16 17:27:23 +0300

    1 Jautajums

--- Eksamena darbs.cpp
+++ Eksamena darbs.cpp
@@ -11,3 +11,9 @@ int main(){
     int rezultats = 0; //Rezultats uz cik jautajumiem atbildeji pareizi
     int nepareizi = 0;//Rezultats uz cik jautajumiem atbildeji nepareizi
     char atbilde; //Pareiza atbilde uz jautajumu
+    
+     cout << "Ka pareizi jatver datnes? (divas atbildes)"<< endl;//1. jautajums un atbildes.
+    cout << "A) datne.open(datne.txt,ios::out); "<< endl; 
+    cout << "B) datne.open(datne.ios::open)" << endl;
+    cout << "C) datne.open(datne.txt,ios::app); "<< endl;
+    cout << "D) datne.open(datne.txt:app::open)" << endl;
