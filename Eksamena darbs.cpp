969a87bf6c69cef25860f8616e08004f41e31804
Richards Griezitis <ricis1818@gmail.com>
2021-06-16 17:08:12 +0300

    Mainigo pievinosana

--- Eksamena darbs.cpp
+++ Eksamena darbs.cpp
@@ -7,3 +7,7 @@
 using namespace std;
 
 int main(){
+	 int n; //Apzime jautajumu skaitu 
+    int rezultats = 0; //Rezultats uz cik jautajumiem atbildeji pareizi
+    int nepareizi = 0;//Rezultats uz cik jautajumiem atbildeji nepareizi
+    char atbilde; //Pareiza atbilde uz jautajumu
